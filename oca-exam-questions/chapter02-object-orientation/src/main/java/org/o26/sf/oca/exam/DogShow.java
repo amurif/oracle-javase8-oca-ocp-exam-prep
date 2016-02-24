8398ab0befeab877700ea8a0a0aaf759a3bb7a27
FAM <amurif@hotmail.com>
2016-02-24 19:50:05 +0100

    Add unit test

---
+++ oca-exam-questions/chapter02-object-orientation/src/main/java/org/o26/sf/oca/exam/DogShow.java
@@ -0,0 +1,7 @@
+package org.o26.sf.oca.exam;
+
+/**
+ * Created by O26 on 24/02/2016.
+ */
+public class DogShow {
+}
---
+++ oca-exam-questions/chapter02-object-orientation/src/test/java/org/o26/sf/oca/exam/DogShowTest.java
@@ -0,0 +1,28 @@
+package org.o26.sf.oca.exam;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by O26 on 24/02/2016.
+ */
+public class DogShowTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGo() throws Exception {
+
+    }
+}
