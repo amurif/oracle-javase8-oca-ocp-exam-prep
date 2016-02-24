7ea27e3c16a671996a1916f7aa90cb60eaaa656f
FAM <amurif@hotmail.com>
2016-02-24 19:36:37 +0100

    Add unit test

---
+++ oca-exam-questions/chapter02-object-orientation/src/main/java/org/o26/sf/oca/exam/Locomotive.java
@@ -0,0 +1,18 @@
+package org.o26.sf.oca.exam;
+
+/**
+ * Created by O26 on 24/02/2016.
+ */
+public class Locomotive {
+    Locomotive() {
+        main("hi");
+    }
+
+    public static void main(String[] args) {
+        System.out.print("2 ");
+    }
+
+    public static void main(String args) {
+        System.out.print("3 " + args);
+    }
+}
---
+++ oca-exam-questions/chapter02-object-orientation/src/test/java/org/o26/sf/oca/exam/LocomotiveTest.java
@@ -0,0 +1,27 @@
+package org.o26.sf.oca.exam;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by O26 on 24/02/2016.
+ */
+public class LocomotiveTest {
+
+    @Test
+    public void testLocomotiveClass() throws Exception {
+        new Locomotive();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
