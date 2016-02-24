99f8adb968d088dfb239ac7c59b4a40b97471f90
FAM <amurif@hotmail.com>
2016-02-24 18:22:28 +0100

    Add unit test

---
+++ oca-exam-questions/chapter02-object-orientation/src/main/java/org/o26/sf/oca/exam/Hawk.java
@@ -0,0 +1,46 @@
+package org.o26.sf.oca.exam;
+
+/**
+ * Created by O26 on 24/02/2016.
+ */
+class Bird {
+    {
+        System.out.print("b1 ");
+    }
+
+    public Bird() {
+        System.out.print("b2 ");
+    }
+    static {
+        System.out.print("b3 ");
+    }
+}
+
+class Raptor extends Bird {
+    static {
+        System.out.print("r1 ");
+    }
+
+    public Raptor() {
+        System.out.print("r2 ");
+    }
+
+    {
+        System.out.print("r3 ");
+    }
+
+    static {
+        System.out.print("r4 ");
+    }
+}
+
+public class Hawk extends Raptor {
+    static {
+        System.out.print("h1 ");
+    }
+    public static void go() {
+        System.out.print("pre ");
+        new Hawk();
+        System.out.println("hawk ");
+    }
+}
